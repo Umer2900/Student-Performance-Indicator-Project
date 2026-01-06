e9f12cbcf96e936cc5b8c3e1088f6f72df04cb33
Umer <janumar2900@gmail.com>
2026-01-06 15:05:47 +0530

    Front End Completed

--- app.py
+++ app.py
@@ -1,50 +1,42 @@
 from flask import Flask, request, render_template
 import numpy as np
 import pandas as pd
-    
+
 from sklearn.preprocessing import StandardScaler
 from src.pipeline.predict_pipeline import CustomData, PredictPipeline
 
 application = Flask(__name__)
 app = application
 
-## Route for home page
-@app.route('/')
-def index():
-    return render_template('index.html') 
-
-@app.route('/predictdata', methods=['GET', 'POST'])
+## Route for index page
+@app.route('/', methods=['GET', 'POST'])
 def predict_datapoint():
     try:
-        if request.method == 'GET':
-            return render_template('home.html')
+        if request.method == 'POST':
 
-        # Get form data
-        data = CustomData(
-            gender=request.form.get('gender'),
-            race_ethnicity=request.form.get('race_ethnicity'),
-            parental_level_of_education=request.form.get('parental_level_of_education'),
-            lunch=request.form.get('lunch'),
-            test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('reading_score')),
-            writing_score=float(request.form.get('writing_score'))
-        )
+            data = CustomData(
+                gender=request.form.get('gender'),
+                race_ethnicity=request.form.get('race_ethnicity'),
+                parental_level_of_education=request.form.get('parental_level_of_education'),
+                lunch=request.form.get('lunch'),
+                test_preparation_course=request.form.get('test_preparation_course'),
+                reading_score=float(request.form.get('reading_score')),
+                writing_score=float(request.form.get('writing_score'))
+            )
 
-        # Check for missing values
-        if None in [data.gender, data.race_ethnicity, data.parental_level_of_education,
-                    data.lunch, data.test_preparation_course]:
-            return "Error: Please fill all the fields!"
+            pred_df = data.get_data_as_data_frame()
+            predict_pipeline = PredictPipeline()
+            results = predict_pipeline.predict(pred_df)
 
-        pred_df = data.get_data_as_data_frame()
+            final_result = round(float(results[0]), 2)
 
-        predict_pipeline = PredictPipeline()
-        results = predict_pipeline.predict(pred_df)
+            return render_template('index.html', results=final_result, form_data=request.form)
 
-        return render_template('home.html', results=float(results[0]))
+        return render_template('index.html')
 
     except Exception as e:
-        print("ERROR:", e)
         return f"Error occurred: {e}"
 
+
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", debug=True)         #  http://127.0.0.1:5000/
