813b5fd9217edd790634bff9a613003729ecf11c
Umer <janumar2900@gmail.com>
2026-01-06 12:36:39 +0530

    model training + hyperparameter tuning

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,12 +4,14 @@ import pandas as pd
 from src.logger import logging
 from src.exception import CustomException
 
-from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
+from sklearn.model_selection import train_test_split
 
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -53,3 +55,6 @@ if __name__=="__main__":
 
     data_transformation = DataTransformation()
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
+
+    modeltrainer=ModelTrainer()
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
